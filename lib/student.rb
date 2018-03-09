aabe94a62f84089c1c3718e15b6b881e1150657f
Antonio Reid <read.musik@gmail.com>
2018-03-09 16:58:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,4 +81,15 @@ class Student
 
     DB[:conn].execute(sql)
   end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL 
+      SELECT *
+      FROM students 
+      WHERE id < ?
+    SQL
+
+    DB[:conn].execute(sql, x)
+      
+  end
 end
